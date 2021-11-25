1a55b3b2d0db1ba6de3144b3ce17df97d988d546
Kelvin Garcia <kelvinrgarcia98@gmail.com>
2021-11-25 18:14:04 -0300

    incluindo configurações para rodar num web container tomcat

--- src/main/java/com/kelvin/sorteio/SorteioApplication.java
+++ src/main/java/com/kelvin/sorteio/SorteioApplication.java
@@ -2,11 +2,18 @@ package com.kelvin.sorteio;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 import org.springframework.data.mongodb.repository.config.EnableMongoRepositories;
 
 @SpringBootApplication
 @EnableMongoRepositories
-public class SorteioApplication {
+public class SorteioApplication extends SpringBootServletInitializer {
+
+	@Override
+	protected SpringApplicationBuilder configure(SpringApplicationBuilder builder) {
+		return builder.sources(SorteioApplication.class);
+	}
 
 	public static void main(String[] args) {
 		SpringApplication.run(SorteioApplication.class, args);
